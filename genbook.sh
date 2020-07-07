d05a70d7ae38f658dcf91e95cd18696133eef129
Eric <eric@jj.me>
2020-07-07 13:40:37 +0800

    /home/eric/gitrepo/github/AWS-Learning-Doc/book_1,gitbook buil

--- genbook.sh
+++ genbook.sh
@@ -9,26 +9,25 @@ git checkout master
 
 # build gitbook
 find $BASEDIR -maxdepth 1 -type d -name "book_*"|while read b;do
-
 cd $b
 gitbook build
-
-
-git checkout gh-pages
-cp -r _book/* .
+cd $BASEDIR
 git add .
-git commit -m "$b,$1"
-#git push -u origin gh-pages
-git checkout master
+git commit -m "$b,gitbook buil"
 done
 
-# commit master
-cd $BASEDIR
-git checkout master
-git add .
-git commit -m $1
+exit 1
+# push master
 git push -u origin master
 
-# commit gh-pages
 git checkout gh-pages
+# add gitbook to gh-pages
+find $BASEDIR -maxdepth 1 -type d -name "book_*"|while read b;do
+cd $b
+git add .
+cp -r _book/* .
+git commit -m "$b,gitbook buil"
+done
+
+# push gh-pages
 git push -u origin gh-pages
