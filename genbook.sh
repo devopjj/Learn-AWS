da2baffece2562718c5eb5849d43135a34bb924f
Eric <eric@jj.me>
2020-07-07 12:13:36 +0800

    test123

--- genbook.sh
+++ genbook.sh
@@ -9,21 +9,26 @@ git checkout master
 
 # build gitbook
 find $BASEDIR -maxdepth 1 -type d -name "book_*"|while read b;do
-cd $b
-pwd
-gitbook build
-#
 
-#git checkout gh-pages
+cd $b
+gitbook build
+
+
+git checkout gh-pages
 cp -r _book/* .
-#git add .
-#git commit -m $1
+git add .
+git commit -m $1
 #git push -u origin gh-pages
-#git checkout master
+git checkout master
 done
 
 # commit master
+git checkout master
 cd $BASEDIR
 git add .
 git commit -m $1
 git push -u origin master
+
+# commit gh-pages
+git checkout gh-pages
+git push -u origin gh-pages
