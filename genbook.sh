96771ec6117675fe10a362b97c4daea3f2754ca9
Eric <eric@jj.me>
2020-07-07 15:26:43 +0800

    edit genbook.sh

--- genbook.sh
+++ genbook.sh
@@ -11,7 +11,6 @@ git checkout master
 find $BASEDIR -maxdepth 1 -type d -name "book_*"|while read b;do
 cd $b
 gitbook build
-cd $BASEDIR
 git add .
 bookname=`echo ${b#*$BASEDIR/}`
 tag=`date '+%Y%m%d %H:%M'`
@@ -19,12 +18,10 @@ git commit -m "$bookname,gitbook build at $tag"
 done
 
 # push master
-
 git push -u origin master
 
 
 
-exit 1
 # build gh-pages
 git checkout gh-pages
 
@@ -36,17 +33,14 @@ git rm --cached -r .
 git clean -df
 rm -rf *~
 
-git add .
 cp -r _book/* .
+git add .
 bookname=`echo ${b#*$BASEDIR/}`
 tag=`date '+%Y%m%d %H:%M'`
 git commit -m "$bokname generate on $tag"
 done
 
 # push gh-pages
-git add .
-tag=`date '+%Y%m%d %H:%M'`
-git commit -m " gh-pages build at $tag"
 git push -u origin gh-pages
 
 git checkout master
