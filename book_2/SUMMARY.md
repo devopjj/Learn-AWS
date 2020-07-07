d148fead7f62abe5afae244b67b39588e77ec4dc
Eric <eric@jj.me>
2020-07-07 14:06:34 +0800

    book_1,gitbook build at 20200707 14:06

--- book_2/SUMMARY.md
+++ book_2/SUMMARY.md
@@ -3,4 +3,4 @@ book2
 
 * [book2 Introduction](README.md)
 
-2020-07-07 , 13:49
+#### 2 2020-07-07 , 14:05
--- genbook.sh
+++ genbook.sh
@@ -5,9 +5,9 @@ BASEDIR=`pwd`
 fi
 
 
+# build gitbook
 git checkout master
 
-# build gitbook
 find $BASEDIR -maxdepth 1 -type d -name "book_*"|while read b;do
 cd $b
 gitbook build
@@ -19,16 +19,32 @@ git commit -m "$bookname,gitbook build at $tag"
 done
 
 # push master
+git add .
+git commit -m " master build at $tag"
 git push -u origin master
+
 exit 1
+# build gh-pages
 git checkout gh-pages
+
 # add gitbook to gh-pages
 find $BASEDIR -maxdepth 1 -type d -name "book_*"|while read b;do
 cd $b
+# clear cache ,temp
+git rm --cached -r .
+git clean -df
+rm -rf *~
+
 git add .
 cp -r _book/* .
-git commit -m "$b,gitbook buil"
+bookname=`echo ${b#*$BASEDIR/}`
+tag=`date '+%Y%m%d %H:%M'`
+git commit -m "$bokname generate on $tag"
 done
 
 # push gh-pages
+git add .
+git commit -m " gh-pages build at $tag"
 git push -u origin gh-pages
+
+git checkout master
