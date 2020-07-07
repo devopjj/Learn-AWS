742f44e53503aabd3cf8ea6100bbab0f86ab6b0c
Eric <eric@jj.me>
2020-07-07 15:41:50 +0800

    aws-ec2

--- genbook.sh
+++ genbook.sh
@@ -8,10 +8,9 @@ fi
 # build gitbook
 git checkout master
 
-find $BASEDIR -maxdepth 1 -type d -name "book_*"|while read b;do
+find $BASEDIR -maxdepth 1 -type d -name "mormoraws-*"|while read b;do
 cd $b
 gitbook build
-cd $BASEDIR
 git add .
 bookname=`echo ${b#*$BASEDIR/}`
 tag=`date '+%Y%m%d %H:%M'`
@@ -19,32 +18,29 @@ git commit -m "$bookname,gitbook build at $tag"
 done
 
 # push master
-git add .
-git commit -m " master build at $tag"
 git push -u origin master
+
+
 
 # build gh-pages
 git checkout gh-pages
-
+git add .
 # add gitbook to gh-pages
-find $BASEDIR -maxdepth 1 -type d -name "book_*"|while read b;do
+find $BASEDIR -maxdepth 1 -type d -name "aws-*"|while read b;do
 cd $b
 # clear cache ,temp
 git rm --cached -r .
 git clean -df
 rm -rf *~
 
-git add .
 cp -r _book/* .
+git add .
 bookname=`echo ${b#*$BASEDIR/}`
 tag=`date '+%Y%m%d %H:%M'`
 git commit -m "$bokname generate on $tag"
 done
 
 # push gh-pages
-git add .
-tag=`date '+%Y%m%d %H:%M'`
-git commit -m " gh-pages build at $tag"
 git push -u origin gh-pages
 
 git checkout master
